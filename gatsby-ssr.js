db37dfe20d8ec1287e824643da315e53331e1020
Pieter Wolmarans <pieter@pixelume.com>
2021-06-25 20:20:04 +0200

    Addressed FOUT by importing Globalstyle in wrapPageElement

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import WrapRoot from './src/components/WrapRoot';
 import Layout from './src/components/Layout/Layout';
+import GlobalStyle from './src/styles/GlobalStyle';
 
 export const wrapRootElement = ({element}) => (
   <WrapRoot>
@@ -11,5 +12,10 @@ export const wrapRootElement = ({element}) => (
 export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
-  return <Layout {...props}>{element}</Layout>
+  return (
+    <>
+      <GlobalStyle/>
+      <Layout {...props}>{element}</Layout>
+    </>
+  )
 }
